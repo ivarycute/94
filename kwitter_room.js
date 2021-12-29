35e625b4d1d57f1c48710e46fa608388b430aa25
ivary <82458747+ivarycute@users.noreply.github.com>
2021-12-29 14:37:20 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCB1nfV-TprWQheKUNa7PCybTAjTHpCb9E",
+    authDomain: "kwitter-3953d.firebaseapp.com",
+    databaseURL: "https://kwitter-3953d-default-rtdb.firebaseio.com",
+    projectId: "kwitter-3953d",
+    storageBucket: "kwitter-3953d.appspot.com",
+    messagingSenderId: "1039554963168",
+    appId: "1:1039554963168:web:1c8f140b13c6e611f6d285",
+    measurementId: "G-ZYB70BX4ZZ"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
